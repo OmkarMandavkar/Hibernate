eaed15b87de1f2502d235d497a3f14aaa7b789a6
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-11-23 19:07:50 +0530

    Add files via upload

---
+++ Uni-Many-To-Many/src/main/java/FetchRecord.java
@@ -0,0 +1,38 @@
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class FetchRecord {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("UniManyToMany");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		et.begin();
+
+		StudentData s1 = em.find(StudentData.class, 102);
+		et.commit();
+
+		if (s1 != null) {
+			System.out.println("\n\n------    StudentData    ------");
+			System.out.println(s1.getId());
+			System.out.println(s1.getName());
+			System.out.println(s1.getClass());
+
+			List<Subject> sub = s1.getSubjects();
+			System.out.println("------------	Subjects	-------------");
+			for (Subject subject : sub) {
+				System.out.println(subject);
+			}
+		} else {
+			System.out.println("No Data Found");
+		}
+
+	}
+
+}
