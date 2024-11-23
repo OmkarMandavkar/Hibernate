43bfd535e9254528e4180a6e5a388500d70d29d0
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-11-23 19:08:15 +0530

    Add files via upload

---
+++ GenerationType-Table/src/main/java/Account_TABLE.java
@@ -0,0 +1,92 @@
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "account_table")
+public class Account_TABLE {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = "table_gen")
+	@TableGenerator(name="table_gen", table = "id_generator_table", initialValue = 99, allocationSize = 50)
+	private int id;
+
+	@Column(name = "username")
+	private String name;
+
+	@Column(unique = true, nullable = false, updatable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private long phone;
+
+	@CreationTimestamp
+	private LocalDateTime createdDate;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedDate;
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", createdDate="
+				+ createdDate + ", updatedDate=" + updatedDate + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+}
---
+++ GenerationType-Table/src/main/java/InsertRecord.java
@@ -0,0 +1,37 @@
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class InsertRecord {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("GTtable");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+//		Account_TABLE a1 = new Account_TABLE();
+//		a1.setName("Zeus");
+//		a1.setEmail("zeus@gmail.com");
+//		a1.setPhone(9876543210L);
+
+//		Account_TABLE a1 = new Account_TABLE();
+//		a1.setName("Krypto");
+//		a1.setEmail("krypto@gmail.com");
+//		a1.setPhone(9999443366L);
+
+		Account_TABLE a1 = new Account_TABLE();
+		a1.setName("Cloud9");
+		a1.setEmail("cloud9@gmail.com");
+		a1.setPhone(9988776655L);
+
+
+		et.begin();
+		em.persist(a1);
+		et.commit();
+		
+		System.out.println("Records Saved");
+	}
+}
