0edabc54259d40d86b379ce89068d8ee89ca3790
Omkar Mandavkar <73053306+OmkarMandavkar@users.noreply.github.com>
2024-11-23 19:13:09 +0530

    Add files via upload

---
+++ Bi-Many-To-Many/src/main/java/DeleteByStudent.java
@@ -0,0 +1,38 @@
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class DeleteByStudent {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("BiManyToMany");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		et.begin();
+
+		Student student = em.find(Student.class, 3);
+
+		if (student != null) {
+
+			List<Subject> subject = student.getSubjects();
+
+			for (Subject sub : subject) {
+				sub.setStudents(null);
+				em.merge(sub);
+			}
+			
+			em.remove(student);
+			et.commit();
+			System.out.println("Record Deleted");
+			
+		} else {
+			System.out.println("No Record Found");
+		}
+	}
+
+}
---
+++ Bi-Many-To-Many/src/main/java/DeleteBySubject.java
@@ -0,0 +1,38 @@
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class DeleteBySubject {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("BiManyToMany");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+
+		et.begin();
+
+		Subject subject = em.find(Subject.class, 104);
+
+		if (subject != null) {
+
+			List<Student> student = subject.getStudents();
+
+			for (Student stud : student) {
+				stud.setSubjects(null);
+				em.merge(stud);
+			}
+
+			em.remove(subject);
+			et.commit();
+			System.out.println("Record Deleted");
+
+		} else {
+			System.out.println("No Record Found");
+		}
+	}
+
+}
